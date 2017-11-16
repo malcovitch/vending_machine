7aff867975b22deceaba2efc1dd3a6bfd14ee0e5
Malcom.Bailey <malcolm.bailey@dlyticstech.net>
2017-11-16 16:06:22 +0000

    Initial commit.

---
+++ src/main/java/com/malcolm/tesco_vend/ItemManager.java
@@ -0,0 +1,6 @@
+package com.malcolm.tesco_vend;
+
+public class ItemManager {
+
+
+}
---
+++ src/main/java/com/malcolm/tesco_vend/VendingMachine.java
@@ -0,0 +1,33 @@
+package com.malcolm.tesco_vend;
+
+/**
+ * Encapsulates the state of a vending machine and the operations that can be performed on it
+ */
+public class VendingMachine {
+	
+	public VendingMachine() {
+		super();
+	}
+	
+	public boolean isOn() {
+		return false;
+	}
+	
+	public void setOn() {
+	}
+	
+	public void setOff() {
+	}
+
+	public void coinReturn() {
+
+	}
+
+	public void insertMoney(int denomination) {
+
+	}
+
+	public Item getItem(String itemId) {
+
+	}
+}
